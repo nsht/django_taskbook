84097753aebf296b8e434d517e565c4472c745eb
Nishit Mohanan <nishitmohanan@gmail.com>
2018-10-31 01:15:01 +0530

    Added ability to add subitem from  frontend

--- todoapp/todo/static/js/todo.js
+++ todoapp/todo/static/js/todo.js
@@ -1,3 +1,5 @@
+// TODO need to refactor messy code and hacks
+
 $(document).ready(function(){
     // Check and uncheck TodoItems
     $('.completed-checkbox').click(function(){
@@ -84,14 +86,38 @@ $(document).ready(function(){
         });
     });
 
+    $(document).keypress(function(e) {
 
+        if(e.which == 13) {
+            if ($('.new-todo-text-box').is(":focus")){
+                var csrftoken = $('[name="csrfmiddlewaretoken"]').val()
+                var data = {
+                    csrfmiddlewaretoken: csrftoken,
+                    todo_item_id:$('.new-todo-text-box').attr('data-todoid'),
+                    sub_item:$('.new-todo-text-box').val()
+                }
+                todo_item_id = $('.new-todo-text-box').attr('data-todoid');
+                var lastChar = todo_item_id[todo_item_id.length -1];
+                var parent = $('.new-todo-text-box').parent();
 
+                $.post("/new-sub-item",data).done(function( result ) {
+                console.log("ok");
+                $('.new-todo-text-box').remove();
+                parent.append(result);
+                append_new_subitem_textbox(lastChar)
 
-});
+                })
+            }
+        }
+    });
+
 
 // Tried to use plain js instead of jquery
+
+// Shortcut Functions
 document.onkeyup = function(e) {
-    if(e.which == 78){
+    // 78 = n (popup modal) and no text box active
+    if(e.which == 78 && !$(e.target).closest("input, textarea").length){
         if(!document.getElementById('new_todo_modal').classList.contains("is-active")) {
             document.getElementById('new_todo_modal').classList.add("is-active");
             document.getElementById('id_todo_item').value = "";
@@ -99,9 +125,63 @@ document.onkeyup = function(e) {
         }
     }
     else if(e.which == 27){
+    // 23 = escape (exit modal)
         if(document.getElementById('new_todo_modal').classList.contains("is-active")) {
             document.getElementById('new_todo_modal').classList.remove("is-active");
         }
+        // remove textbox to add subitem
+        else if($(e.target).closest("input, textarea").length){
+                $('.new-todo-text-box').remove();
+        }
     }
-
 }
+
+// new sub todo item functions
+var aclick = document.getElementsByClassName('new-sub-item');
+
+Array.from(aclick).forEach(function(element) {
+    console.log(element);
+    element.addEventListener("click", handleClick,false);
+  });
+
+
+function handleClick(){
+    // Create new span and text
+    var new_list_item = document.createElement("span");
+    var textbox = document.createElement("input");
+    textbox.setAttribute("type", "text");
+    textbox.setAttribute("name","subitem");
+    textbox.setAttribute("data-todoid",this.id);
+    textbox.setAttribute("class",'new-todo-text-box')
+    // select the required elements
+    var parent_id = this.getAttribute('data-todoid');
+    var parent_element = document.getElementById(parent_id);
+    var child_list = parent_element.querySelector('.sub-item');
+    // append textbox to list
+    new_list_item.appendChild(textbox);
+    child_list.appendChild(new_list_item);
+    child_list.querySelector('.new-todo-text-box').focus()
+  }
+
+function append_new_subitem_textbox(id){
+    // Create new span and text
+    var new_list_item = document.createElement("span");
+    var textbox = document.createElement("input");
+    textbox.setAttribute("type", "text");
+    textbox.setAttribute("name","subitem");
+    textbox.setAttribute("data-todoid",id);
+    textbox.setAttribute("class",'new-todo-text-box')
+    // select the required elements
+    var parent_id = id;
+    var parent_element = document.getElementById(parent_id);
+    var child_list = parent_element.querySelector('.sub-item');
+    // append textbox to list
+    new_list_item.appendChild(textbox);
+    child_list.appendChild(new_list_item);
+    child_list.querySelector('.new-todo-text-box').focus()
+}
+
+
+
+
+});
--- todoapp/todo/urls.py
+++ todoapp/todo/urls.py
@@ -5,8 +5,12 @@ from . import views
 app_name = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new', views.new_todo, name='new_todo'),
+    path('new', views.new_todo_page, name='new_todo'),
     path('add', views.add_todo, name='add_todo'),
     path('toggle-completion', views.toggle_completion, name='toggle_completion'),
     path('toggle-star', views.toggle_stars, name='toggle_stars'),
+    path('toggle-star', views.toggle_stars, name='toggle_stars'),
+    path('new-sub-item', views.new_sub_item, name='new_sub_item'),
+
+
 ]
