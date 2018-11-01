6327654fea7c175b8f527293c4ef8673db5d6137
Nishit Mohanan <nishitmohanan@gmail.com>
2018-11-02 01:18:32 +0530

    removed duplicate url

--- todoapp/todo/urls.py
+++ todoapp/todo/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('add', views.add_todo, name='add_todo'),
     path('toggle-completion', views.toggle_completion, name='toggle_completion'),
     path('toggle-star', views.toggle_stars, name='toggle_stars'),
-    path('toggle-star', views.toggle_stars, name='toggle_stars'),
     path('new-sub-item', views.new_sub_item, name='new_sub_item'),
 ]
