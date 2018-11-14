af75b0192b55d09c4bc94115ee881bc01adea2c1
Nishit Mohanan <nishitmohanan@gmail.com>
2018-11-14 23:34:28 +0530

    code format changes and cleanup

--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -7,42 +7,36 @@ from .models import TodoItem, SubItem
 from django.utils import timezone
 
 from .forms import TodoItemForm
+
 # Create your views here.
 
 
 def index(request):
     form = TodoItemForm()
-    todos = TodoItem.objects.prefetch_related('subitem_set').all()
-    context = {
-        'todos': todos,
-        'form': form
-    }
-    return render(request, 'todo/index.html', context=context)
+    todos = TodoItem.objects.prefetch_related("subitem_set").all()
+    context = {"todos": todos, "form": form}
+    return render(request, "todo/index.html", context=context)
 
 
 def new_todo_page(request):
     form = TodoItemForm()
 
-    return render(request, 'todo/new_todo.html', {'form': form})
+    return render(request, "todo/new_todo.html", {"form": form})
 
 
 def add_todo(request):
-    todo_item = request.POST['todo_item']
-    new_todo = TodoItem.objects.create(
-        todo_item=todo_item, created=timezone.now(), status=0
-    )
+    todo_item = request.POST["todo_item"]
+    new_todo = TodoItem.objects.create(todo_item=todo_item, created=timezone.now(), status=0)
     new_todo.save()
-    context = {
-        "todo": new_todo
-    }
-    return render(request, 'todo/single_todo.html', context)
+    context = {"todo": new_todo}
+    return render(request, "todo/single_todo.html", context)
 
 
 def toggle_completion(request):
-    if request.POST['type'] == 'subitem':
-        todo = get_object_or_404(SubItem, id=request.POST['id'])
+    if request.POST["type"] == "subitem":
+        todo = get_object_or_404(SubItem, id=request.POST["id"])
     else:
-        todo = get_object_or_404(TodoItem, id=request.POST['id'])
+        todo = get_object_or_404(TodoItem, id=request.POST["id"])
     if todo:
         if todo.status == 0:
             todo.status = 1
@@ -53,7 +47,7 @@ def toggle_completion(request):
 
 
 def toggle_stars(request):
-    todo = get_object_or_404(TodoItem, id=request.POST['id'])
+    todo = get_object_or_404(TodoItem, id=request.POST["id"])
     if todo:
         if todo.starred == 0:
             todo.starred = 1
@@ -62,14 +56,15 @@ def toggle_stars(request):
         todo.save()
         return HttpResponse(status=200)
 
+
 def new_sub_item(request):
-    id=request.POST['todo_item_id']
-    id = id.split('_')[-1]
+    id = request.POST["todo_item_id"]
+    id = id.split("_")[-1]
     todo = get_object_or_404(TodoItem, id=id)
     sub_item = todo.subitem_set.create(
-        sub_item = request.POST['sub_item'],sub_created=timezone.now()
+        sub_item=request.POST["sub_item"], sub_created=timezone.now()
     )
     print(sub_item)
-    context = {'subitem':sub_item}
-    return render(request,'todo/single_sub_item.html', context)
+    context = {"subitem": sub_item}
+    return render(request, "todo/single_sub_item.html", context)
     # return HttpResponse(status=200)
